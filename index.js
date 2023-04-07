12525c014b2e703accbbe2a8621aba2dcf91e651
CassieRasugu <cassierasugu@gmail.com>
2023-04-07 18:03:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return "This Saturday I will go and ${activity}";
+}
+function mondayWork(activity = "go to the office"){
+    return "This Monday i will go ${activity}";
+}
+function wrapAdjective(flair = "*")
+return function(adjective = "special") {
+    return "you are ${flair}${adjective}${flair}!";
+}
+console.log(encouragingPromptFunction(" A consitant hardworking amazing Lady"));
